1103851a07c7688fad8852ff59300204738ee27c
Seventyy <seventyyyyy@gmail.com>
2023-12-09 12:52:11 +0100

    things changed (no way)

--- rssi-localisation-visualosation/calculator.gd
+++ rssi-localisation-visualosation/calculator.gd
@@ -4,8 +4,8 @@ extends Node2D
 @onready var access_point_a:AccessPoint = $AccessPointA
 @onready var access_point_b:AccessPoint = $AccessPointB
 @onready var access_point_c:AccessPoint = $AccessPointC
-@onready var resoult:Sprite2D = $Resoult
 
+#region stregths depracated
 #@export var strength_a:float:
 	#set(val):
 		#strength_a = val
@@ -21,6 +21,7 @@ extends Node2D
 		#strength_c = val
 		#access_point_c.strength = strength_c
 		#queue_redraw()
+#endregion
 
 @export var intercept:float = -64
 @export var path_loss_exponent:float = 3
@@ -46,18 +47,25 @@ extends Node2D
 		show_primary_intersections = val
 		queue_redraw()
 
-@export var reset_positions:bool:
+@export var reset_positions_old:bool:
 	set(val):
 		access_point_a.position = Vector2(6000, 4000) 
 		access_point_b.position = Vector2(6000, 0)
 		access_point_c.position = Vector2(0, 2000)
-		#reset_handles()
+@export var reset_positions:bool:
+	set(val):
+		access_point_a.position = Vector2(0, 0) 
+		access_point_b.position = Vector2(6000, 2000)
+		access_point_c.position = Vector2(0, 4000)
 @export var reset_strength:bool:
 	set(val):
 		access_point_a.strength = 2000
 		access_point_b.strength = 2500
 		access_point_c.strength = 4500
 		reset_handles()
+@export var redraw:bool:
+	set(val):
+		queue_redraw()
 
 func reset_handles() -> void:
 	access_point_a.handle.position = Vector2.UP * access_point_a.strength
@@ -97,21 +105,21 @@ func reset_handles() -> void:
 	#var access_point_c:Array[int] 
 #endregion
 #region console_print
-@export var print_calculations:bool:
-	set(val):
-		print("\nall intersections:\n",
-			calculate_all_intersections([access_point_a, access_point_b, access_point_c])
-			)
-		print("\nthree closest intersections:\n", 
-			calculate_smallest_perimeter_triplet(
-			calculate_all_intersections([access_point_a, access_point_b, access_point_c])
-			))
-		print("\navrage of three closest intersections:\n", 
-			calculate_centroid(
-			calculate_smallest_perimeter_triplet(
-			calculate_all_intersections([access_point_a, access_point_b, access_point_c])
-			)))
-		queue_redraw()
+#@export var print_calculations:bool:
+	#set(val):
+		#print("\nall intersections:\n",
+			#calculate_all_intersections([access_point_a, access_point_b, access_point_c])
+			#)
+		#print("\nthree closest intersections:\n", 
+			#calculate_smallest_perimeter_triplet(
+			#calculate_all_intersections([access_point_a, access_point_b, access_point_c])
+			#))
+		#print("\navrage of three closest intersections:\n", 
+			#calculate_centroid(
+			#calculate_smallest_perimeter_triplet(
+			#calculate_all_intersections([access_point_a, access_point_b, access_point_c])
+			#)))
+		#queue_redraw()
 #endregion
 #region old_code
 #func calculate_all_intersections(circles: Array[AccessPoint]) -> Array[Vector2]:
@@ -147,12 +155,25 @@ func calculate_intersections(circle1:AccessPoint, circle2:AccessPoint) -> Array[
 #  circle2.position.x, circle2.position.y, circle2.strength = circle2
 	var d:float = sqrt((circle2.position.x - circle1.position.x)**2 + (circle2.position.y - circle1.position.y)**2)
 	
-	if d > circle1.strength + circle2.strength or d < abs(circle1.strength - circle2.strength):
+	#if d > circle1.strength + circle2.strength or d < abs(circle1.strength - circle2.strength):
+		#return [
+			#circle1.position + circle1.position.direction_to(circle2.position) * \
+			#circle1.strength / (circle1.strength + circle2.strength) * \
+			#circle1.position.distance_to(circle2.position)
+		#]
+	
+	if d > circle1.strength + circle2.strength:
+		return [
+			circle1.position + circle1.position.direction_to(circle2.position) * \
+			circle1.strength / (circle1.strength + circle2.strength) * \
+			circle1.position.distance_to(circle2.position)
+		]
+	elif d < abs(circle1.strength - circle2.strength):
 		return [
 			circle1.position + circle1.position.direction_to(circle2.position) * circle1.strength,
 			circle2.position + circle2.position.direction_to(circle1.position) * circle2.strength
-			]
-
+		] 
+	
 	var a:float = (circle1.strength**2 - circle2.strength**2 + d**2) / (2 * d)
 	var h:float = sqrt(circle1.strength**2 - a**2)
 	
@@ -164,17 +185,17 @@ func calculate_intersections(circle1:AccessPoint, circle2:AccessPoint) -> Array[
 	
 	var x4:float = x2 - h * (circle2.position.y - circle1.position.y) / d
 	var y4:float = y2 + h * (circle2.position.x - circle1.position.x) / d
-
+	
 	return [Vector2(x3, y3), Vector2(x4, y4)]
 
-func calculate_all_intersections(circles: Array[AccessPoint]) -> Array[Vector2]:
-	var intersections:Array[Vector2] 
-	
-	intersections += calculate_intersections(circles[0], circles[1])
-	intersections += calculate_intersections(circles[1], circles[2])
-	intersections += calculate_intersections(circles[2], circles[0])
-	
-	return intersections
+#func calculate_all_intersections(circles: Array[AccessPoint]) -> Array[Array]:
+	#var intersections:Array[Array] 
+	#
+	#intersections.append(calculate_intersections(circles[0], circles[1]))
+	#intersections.append(calculate_intersections(circles[1], circles[2]))
+	#intersections.append(calculate_intersections(circles[2], circles[0]))
+	#
+	#return intersections
 
 func calculate_distance(point1: Vector2, point2: Vector2) -> float:
 	return point1.distance_to(point2)
@@ -185,30 +206,22 @@ func calculate_perimeter(points:Array[Vector2]) -> float:
 		calculate_distance(points[1], points[2]) + \
 		calculate_distance(points[2], points[0])
 
-func calculate_smallest_perimeter_triplet(points: Array[Vector2]) -> Array[Vector2]:
-	var valid_triplets:Array[Vector2] = [
-		points[0], points[2], points[4],
-		points[0], points[2], points[5],
-		points[0], points[3], points[4],
-		points[0], points[3], points[5],
-		points[1], points[2], points[4],
-		points[1], points[2], points[5],
-		points[1], points[3], points[4],
-		points[1], points[3], points[5],
-	]
+func calculate_smallest_perimeter_triplet(
+		intersections_ab: Array[Vector2],
+		intersections_bc: Array[Vector2],
+		intersections_ca: Array[Vector2]
+	) -> Array[Vector2]:
 	
 	var running_minimum:float = INF
 	var running_triplet:Array[Vector2]
-	for i in range(0,24,3):
-		var triplet:Array[Vector2] = [
-			valid_triplets[i],
-			valid_triplets[i+1],
-			valid_triplets[i+2],
-		]
-		var perimeter:float = calculate_perimeter(triplet)
-		if perimeter < running_minimum:
-			running_minimum = perimeter
-			running_triplet = triplet
+	for ab in intersections_ab:
+		for bc in intersections_bc:
+			for ca in intersections_ca:
+				var triplet:Array[Vector2] = [ab, bc, ca]
+				var perimeter:float = calculate_perimeter(triplet)
+				if perimeter < running_minimum:
+					running_minimum = perimeter
+					running_triplet = triplet
 	
 	return running_triplet
 
@@ -221,12 +234,16 @@ func _draw() -> void:
 	if show_reference_path:
 		draw_polyline(reference_path_closed, Color.GREEN)
 	
+	var intersections_ab: Array[Vector2] = calculate_intersections(access_point_a, access_point_b)
+	var intersections_bc: Array[Vector2] = calculate_intersections(access_point_b, access_point_c)
+	var intersections_ca: Array[Vector2] = calculate_intersections(access_point_c, access_point_a)
+	
 	var all_intersections:Array[Vector2] = \
-		calculate_all_intersections([access_point_a, access_point_b, access_point_c])
+		intersections_ab + intersections_bc + intersections_ca
 	
 	var smallest_perimeter_triplet:Array[Vector2] = \
 		calculate_smallest_perimeter_triplet(
-		all_intersections
+		intersections_ab, intersections_bc, intersections_ca
 	)
 	
 	var centroid:Vector2 = calculate_centroid( 
@@ -238,8 +255,12 @@ func _draw() -> void:
 			draw_circle(i, 100, Color.DIM_GRAY)
 	
 	if show_intersection_pairs:
-		for i in range(0,6,2):
-			draw_dashed_line(all_intersections[i], all_intersections[i+1], Color.AQUA, -1, 100)
+		if intersections_ab.size() == 2:
+			draw_dashed_line(intersections_ab[0], intersections_ab[1], Color.AQUA, -1, 100)
+		if intersections_bc.size() == 2:
+			draw_dashed_line(intersections_bc[0], intersections_bc[1], Color.AQUA, -1, 100)
+		if intersections_ca.size() == 2:
+			draw_dashed_line(intersections_ca[0], intersections_ca[1], Color.AQUA, -1, 100)
 	
 	if show_triplet_triangle:
 		draw_polygon(PackedVector2Array(smallest_perimeter_triplet), PackedColorArray([Color.LIGHT_CORAL]))
--- test/test.pde
+++ test/test.pde
@@ -4,16 +4,26 @@ float camera_speed = 250.0;
 
 float delta;
 
+
+float rotationAngle;
+float elevationAngle;
+
+float centerX;
+float centerY;
+float centerZ;
+
+
 void setup() {
   size(800, 600, P3D);
-  // camera_position = new PVector(0,0,10);
+  camera_position = new PVector(0,0,400);
   // perspective(PI/3.0, width/height, 0, 1000);
-  camera_normal = new PVector(0, 0, -1000000);
+  camera_normal = new PVector(0, 0, -1);
 }
 
 void draw() {
   delta = 1/frameRate;
   background(200);
+  draw_cubes();
 
   
   // camera(camera_position.x, camera_position.y, camera_position.z);
@@ -23,45 +33,99 @@ void draw() {
   //   0, 1, 0
   // );
   
-  if (updateCamera()){
-    camera(
-      camera_position.x, camera_position.y, camera_position.z,
-      camera_normal.x, camera_normal.y, camera_normal.z,
-      0, 1, 0
-    );
-  }
+  updateCamera();
+  // camera(
+  //   camera_position.x, camera_position.y, camera_position.z,
+  //   camera_normal.x * 1000000, camera_normal.y * 1000000, camera_normal.z * 1000000,
+  //   0, 1, 0
+  // );
 
-  fill(#CCFFAA);
-  box(100);
+  // camera(mouseX, height/2, (height/2) / tan(PI/6), width/2, height/2, 0, 0, 1, 0);
+ 
 
-  translate(100, 50, 0);
-  fill(#AACCFF);
-  box(10);
+  // fill(#CCFFAA);
+  // box(100);
+
+  // translate(100, 50, 0);
+  // fill(#AACCFF);
+  // box(10);
   
 }
 
-boolean updateCamera() {
-  if (keyPressed) {
-    if (key == 'w')      {camera_position.z -= camera_speed * delta; return true;}
-    else if (key == 's') {camera_position.z += camera_speed * delta; return true;}
-    else if (key == 'a') {camera_position.x -= camera_speed * delta; return true;}
-    else if (key == 'd') {camera_position.x += camera_speed * delta; return true;}
-    else if (key == 'z') {camera_position.y -= camera_speed * delta; return true;}
-    else if (key == 'x') {camera_position.y += camera_speed * delta; return true;}
-    else if (key == 'q') {
-      PVector vec = new PVector(camera_normal.x, camera_normal.z);
-      vec.rotate(-PI/2 * delta);
-      println(camera_normal);
-      camera_normal = new PVector(vec.x, camera_normal.y, vec.y);
-      println(camera_normal);
-      return true;
-    }
-    else if (key == 'e') {
-      PVector vec = new PVector(camera_normal.x, camera_normal.z);
-      vec.rotate(PI/2 * delta);
-      camera_normal = new PVector(vec.x, camera_normal.y, vec.y);
-      return true;
-    }
+void updateCamera() {
+ rotationAngle = map(mouseX, 0, width, 0, TWO_PI);
+ elevationAngle = map(mouseY, 0, height, 0, PI);
+
+ centerX = cos(rotationAngle) * sin(elevationAngle);
+ centerY = sin(rotationAngle) * sin(elevationAngle);
+ centerZ = -cos(elevationAngle);
+
+ camera(0, 0, 0, centerX, centerY, centerZ, 0, 0, 1);
+}
+
+
+// void updateCamera() {
+//   if (keyPressed) {
+//     if (key == 'w') {
+//       camera_position = PVector.add(camera_position, PVector.mult(camera_normal.normalize(), camera_speed * delta));
+//     } 
+//     else if (key == 's') {
+//       camera_position = PVector.add(camera_position, PVector.mult(camera_normal.normalize(), -camera_speed * delta));
+//     } 
+//     else if (key == 'a') {
+//       PVector vec = new PVector(camera_normal.x, camera_normal.z);
+//       vec.rotate(-PI/2 * delta);
+//       camera_normal = new PVector(vec.x, camera_normal.y, vec.y);
+//     } 
+//     else if (key == 'd') {
+//       PVector vec = new PVector(camera_normal.x, camera_normal.z);
+//       vec.rotate(PI/2 * delta);
+//       camera_normal = new PVector(vec.x, camera_normal.y, vec.y);
+//     }
+//     else if (key == ' ') {
+//       PVector vec = new PVector(camera_normal.y, camera_normal.z);
+//       vec.rotate(-PI/2 * delta);
+//       camera_normal = new PVector(camera_normal.x, vec.x, vec.y);
+//     } 
+//     else if (keyPressed && key == CODED && keyCode == SHIFT) {
+//       PVector vec = new PVector(camera_normal.y, camera_normal.z);
+//       vec.rotate(PI/2 * delta);
+//       camera_normal = new PVector(camera_normal.x, vec.x, vec.y);
+//     }
+//   }
+// }
+
+
+void draw_cubes(){
+    float[][] cubeProperties = {
+    {50, 50, 50, color(255, 0, 0)},
+    {70, -30, -60, color(0, 255, 0)},
+    {-80, 20, 40, color(0, 0, 255)},
+    {90, -70, -30, color(255, 255, 0)},
+    {-60, 80, -20, color(255, 0, 255)},
+    {30, -90, 60, color(0, 255, 255)},
+    {-40, 110, -50, color(128, 0, 0)},
+    {10, -120, 70, color(0, 128, 0)},
+    {-120, 90, -80, color(0, 0, 128)},
+    {100, 60, 20, color(128, 128, 0)},
+    {-30, -50, 100, color(128, 0, 128)},
+    {80, 40, -110, color(0, 128, 128)},
+    {-70, -10, 30, color(192, 192, 192)},
+    {20, 0, -40, color(128, 128, 128)},
+    {-50, 50, 80, color(255, 165, 0)}
+  };
+  
+  // Draw the cubes
+  for (int i = 0; i < cubeProperties.length; i++) {
+    float x = cubeProperties[i][0];
+    float y = cubeProperties[i][1];
+    float z = cubeProperties[i][2];
+    int col = (int) cubeProperties[i][3];
+    
+    pushMatrix();
+    translate(x, y, z);
+    fill(col);
+    box(40); // Adjust the size of the cubes as needed
+    popMatrix();
   }
-  return false;
 }
